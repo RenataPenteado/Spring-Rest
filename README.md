4cf61574a8870b92a2b150c7b59bb8980293411b
Renata Penteado <renata.penteado@litinow.com>
2019-08-10 15:54:43 +0100

    REST and SpringBoot

---
+++ README.md
@@ -0,0 +1,6 @@
+"# quick-poll" 
+
+Examples SpringBoot and REST
+
+Reference:
+Book Spring+REST - Apress
---
+++ src/main/java/com/apress/QuickPollApplication.java
@@ -0,0 +1,15 @@
+package com.apress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mangofactory.swagger.plugin.EnableSwagger;
+
+@SpringBootApplication
+public class QuickPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickPollApplication.class, args);
+	}
+
+}
